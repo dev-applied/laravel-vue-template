52a7b847053e100387affda416a5c6919c716a3b
Devin345458 <jchan@hingedev.com>
2024-06-26 09:05:45 -0500

    Fix missing import

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
