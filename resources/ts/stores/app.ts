83fd15cdae71335721f3df3cb4f90845b0ed0f48
Diego Ormaza <diego@appliedimagination.com>
2024-12-03 11:21:53 -0500

    minor modification to app.ts

--- resources/ts/stores/app.ts
+++ resources/ts/stores/app.ts
@@ -3,25 +3,32 @@ import { defineStore } from "pinia"
 export const useAppStore = defineStore("app", {
   state: () => {
     return {
-      messages: [] as { type: "Success" | "Error", message: String }[]
+      messages: [] as { type: "Success" | "Error" | "Warning", message: String }[],
+      showNavigation: false as boolean,
     }
   },
   actions: {
-    addError(error: String, timeout: number = 5000) {
+    addError(error: String, timeout: number = 10000) {
       this.messages.push({ type: "Error", message: error })
-      window.scrollTo(0, 0)
       setTimeout(() => {
         this.messages.splice(0, 1)
       }, timeout)
     },
     addSuccess(message: String, timeout: number = 5000) {
       this.messages.push({ type: "Success", message })
-      window.scrollTo(0, 0)
       if (timeout > 0) {
         setTimeout(() => {
           this.messages.splice(0, 1)
         }, timeout)
       }
-    }
+    },
+    addWarning(message: String, timeout: number = 10000) {
+      this.messages.push({ type: "Warning", message })
+      if (timeout > 0) {
+        setTimeout(() => {
+          this.messages.splice(0, 1)
+        }, timeout)
+      }
+    },
   }
 })
