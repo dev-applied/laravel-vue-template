e2b7cae3630aef3dea0c1628801c20944af610d1
ajappliedimagination <aj@appliedimagination.com>
2025-09-11 15:34:42 -0300

    Update dayjs.ts

--- resources/ts/utils/dayjs.ts
+++ resources/ts/utils/dayjs.ts
@@ -21,4 +21,135 @@ dayjs.extend(isTomorrow)
 dayjs.extend(isYesterday)
 dayjs.extend(minMax)
 
+type TimeValue = string | number | undefined | null
+
+function formatTime(value: TimeValue, format: string, local: boolean = true): string {
+  if (!value) {
+    return ""
+  }
+  let date = dayjs.utc(value)
+  if (!date.isValid()) {
+    return ""
+  }
+  if (local) {
+    date = date.local()
+  }
+  return date.format(format)
+}
+
+function full_human_date(time: TimeValue, local: boolean = true): string {
+  return formatTime(time, "dddd, MMMM D, Y", local)
+}
+
+function short_human_date(time: TimeValue, local: boolean = true): string {
+  return formatTime(time, "MMM D, Y", local)
+}
+
+function day(time: TimeValue, local: boolean = true): string {
+  return formatTime(time, "dddd", local)
+}
+
+function full_date_time(time: TimeValue, local: boolean = true): string {
+  return formatTime(time, "dddd, MMMM D, Y h:mm a", local)
+}
+
+function short_date_time(time: TimeValue, local: boolean = true): string {
+  return formatTime(time, "MMM D, Y h:mm a", local)
+}
+
+function time(time: TimeValue, local: boolean = true): string {
+  return formatTime(time, "h:mma", local)
+}
+
+function short_date_time_12(time: TimeValue, local: boolean = false): string {
+  return formatTime(time, "MM/DD/YYYY h:mm a", local)
+}
+
+function short_date_time_24(time: TimeValue, local: boolean = false): string {
+  return formatTime(time, "MM/DD/YYYY HH:mm", local)
+}
+
+function short_date(time: TimeValue, local: boolean = true): string {
+  return formatTime(time, "MM/DD/YYYY", local)
+}
+
+function short_hour(time: TimeValue, local: boolean = true): string {
+  return formatTime(time, "HH:mm", local)
+}
+
+function month_year(time: TimeValue, local: boolean = true): string {
+  return formatTime(time, "MM/YYYY", local)
+}
+
+function time_ago(time: TimeValue, local: boolean = true): string {
+  if (!time) {
+    return ""
+  }
+  let date = dayjs.utc(time)
+  if (!date.isValid()) {
+    return ""
+  }
+  if (local) {
+    date = date.local()
+  }
+  return dayjs.min(date.endOf('day'), dayjs()).fromNow()
+}
+
+function time_future(time: TimeValue, local: boolean = true): string {
+  if (!time) {
+    return ""
+  }
+  let date = dayjs.utc(time)
+  if (!date.isValid()) {
+    return ""
+  }
+  if (local) {
+    date = date.local()
+  }
+  const start = date.clone().startOf('day')
+  const end = date.clone().add(1, 'second').startOf('day')
+
+  if (start.isSame(end)) {
+    return "Today"
+  }
+  return dayjs.max(start).to(end)
+}
+
+function time_calculate(time: TimeValue, local: boolean = true): string {
+  if (!time) {
+    return ""
+  }
+  let date = dayjs.utc(time)
+  if (!date.isValid()) {
+    return ""
+  }
+  if (local) {
+    date = date.local()
+  }
+  if (date.isAfter()) {
+    return time_future(time, local)
+  } else {
+    return time_ago(time, local)
+  }
+}
+
 export default dayjs
+
+export {
+  formatTime,
+  full_human_date,
+  short_human_date,
+  day,
+  full_date_time,
+  short_date_time,
+  time,
+  short_date_time_12,
+  short_date_time_24,
+  short_date,
+  short_hour,
+  month_year,
+  time_ago,
+  time_future,
+  time_calculate,
+  dayjs
+}
