e21cd9f8c80be8ff469c15ee5fc9b797e9ce4b77
Diego Ormaza <diego@appliedimagination.com>
2026-01-20 09:14:37 -0500

    [master] bug fixes

--- resources/ts/composables/useRoute.ts
+++ resources/ts/composables/useRoute.ts
@@ -1,7 +1,9 @@
 import routeTo from '@/plugins/routeTo.ts'
+import {ROUTES} from "@/router/paths"
 
 export default function useAuth() {
   return {
-    route: routeTo
+    route: routeTo,
+    ROUTES
   }
 }
